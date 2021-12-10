6909926249e696fc8e0b4ecb0fdb74a501b7b6b3
Japozeno <94966801+kenryfuzio@users.noreply.github.com>
2021-12-10 10:38:54 -0300

    contagem tabuada
    tabuada de 1 ao 10

--- laco-repeticao/index.js
+++ laco-repeticao/index.js
@@ -1,10 +1,15 @@
+
 document.querySelector("#btn-calcular").addEventListener("click", function
 (){
     let numero = parseInt(document.querySelector('#txtTabuada').value);
+    let tabuada = 0; 
+    document.querySelector('#tabuada').innerHTML = '';
     for (let i = 1; i <= 10; i++){
         tabuada = numero * i;
         let result = document.createElement ('p');
-        result.innerHTML = tabuada; 
+        result.innerHTML = numero +' * '+ i + ' = ' + tabuada; 
+        result.style = 'margin: 0';
+        //result.classList.add('n-0');
         document.querySelector('#tabuada').appendChild(result);
 
     }
@@ -12,3 +17,4 @@ document.querySelector("#btn-calcular").addEventListener("click", function
 
 
 });
+
